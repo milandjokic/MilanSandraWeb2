dfea7b79f87f35613cfe07dbab6ff4f5dee30922
Sandra Coralic <scoralic33@gmail.com>
2019-09-01 02:55:59 +0200

    Stanica(Admin View) Update

--- NewProject/NewProject/src/app/components/stanica/stanica.component.ts
+++ NewProject/NewProject/src/app/components/stanica/stanica.component.ts
@@ -3,6 +3,9 @@ import { FormBuilder, Validators } from '@angular/forms';
 import { FormGroup } from '@angular/forms/src/model';
 import { StanicaService } from 'src/app/services/stanica/stanica.service';
 import { Station } from 'src/app/models/station';
+import { MarkerInfo } from '../map/model/marker-info.model';
+import { GeoLocation } from '../map/model/geolocation';
+import { Polyline } from '../map/model/polyline';
 
 @Component({
   selector: 'app-stanica',
@@ -30,11 +33,18 @@ export class StanicaComponent implements OnInit {
    selectedStation: Station;
    station : Station;
    lines : string[] = [];
+   markerInfo: MarkerInfo;
+   selLine: Polyline;
+   clicked: boolean = false;
 
   constructor(private fb: FormBuilder, private stationService: StanicaService) { this.getStations();}
 
   ngOnInit() {
     this.getStations();
+    this.markerInfo = new MarkerInfo(new GeoLocation(45.242268, 19.842954), 
+    "assets/images/ftn.png",
+    "Jugodrvo" , "" , "http://ftn.uns.ac.rs/691618389/fakultet-tehnickih-nauka");
+    this.selLine = new Polyline([], 'red', { url:"assets/images/autobus.png", scaledSize: {width: 50, height: 50}});
   }
 
   onSelect(event : any){
@@ -97,7 +107,25 @@ export class StanicaComponent implements OnInit {
         }
       );
      
-    }  
+    }
+    MapClicked(event: any)
+    {
+      this.clicked = true;
+      this.selLine = new Polyline([], 'red', { url:"assets/busicon.png", scaledSize: {width: 50, height: 50}});
+      this.selLine.addLocation(new GeoLocation(event.coords.lng, event.coords.lat));
+      this.addStationForm.controls.xCoordinate.setValue(event.coords.lat);
+      this.addStationForm.controls.yCoordinate.setValue(event.coords.lng);
+    }
+    
+    MapClickedEdit(event: any)
+    {
+      
+      this.clicked = true;
+      this.selLine = new Polyline([], 'red', { url:"assets/busicon.png", scaledSize: {width: 50, height: 50}});
+      this.selLine.addLocation(new GeoLocation(event.coords.lng, event.coords.lat));
+      this.editOrRemoveStationForm.controls.xCoordinate.setValue(event.coords.lat);
+      this.editOrRemoveStationForm.controls.yCoordinate.setValue(event.coords.lng);
+    }
   }
 
 
